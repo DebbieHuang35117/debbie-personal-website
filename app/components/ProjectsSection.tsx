55f7940f271f0de6fd46f0ba9f5d17b9a5301ee4
黃芷榆 <huangzhiyu@huangzhiyudeMacBook-Pro.local>
2025-04-07 16:39:06 +0800

    deactivated link function for projects

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -17,7 +17,7 @@ export default function ProjectsSection() {
       period: "Jun. 2024 - Present",
       description: "Implementing Retrieval-Augmented Generation technique to fine-tune LLM to the medical field. Optimizing TAIDE for Taiwan's cultural characteristics.",
       slug: "medical-case-manager",
-      github: "https://github.com/2024-sinica-medLLM",
+      //github: "https://github.com/2024-sinica-medLLM",
       tags: ["LLM", "RAG", "TAIDE"]
     },
     {
@@ -25,7 +25,7 @@ export default function ProjectsSection() {
       period: "Feb. 2024 - Present",
       description: "Modeling and implementing optimization algorithms for production planning in aluminum doors and windows manufacturing.",
       slug: "scenic-ai-manufacturing",
-      github: "https://github.com/DebbieHuang35117/aluminum-manufacturing-production-scheduling",
+      //github: "https://github.com/DebbieHuang35117/aluminum-manufacturing-production-scheduling",
       tags: ["Optimization", "Manufacturing", "AI"]
     },
     {
@@ -33,8 +33,8 @@ export default function ProjectsSection() {
       period: "Apr. 2024 - Jun. 2024",
       description: "A mobile application designed to streamline the photo and video sharing process, enabling users to share life's moments instantly with closed ones.",
       slug: "camera-connect",
-      slug_not_done: true,
-      github: "https://github.com/YouMingYeh/camera-connect",
+      //slug_not_done: true,
+      //github: "https://github.com/YouMingYeh/camera-connect",
       tags: ["Mobile App", "React Native", "Next.js", "Supabase", "Expo"]
     },
     {
@@ -42,7 +42,7 @@ export default function ProjectsSection() {
       period: "Apr. 2024 - Jun. 2024",
       description: "Analyzing shopping basket data to provide recommendations for customers. The project involves data analysis, visualization, and machine learning techniques.",
       slug: "shopping-basket-recommendation-analysis",
-      slug_not_done: true,
+      //slug_not_done: true,
       tags: ["Data Analysis", "Python", "Pandas", "Matplotlib"]
     },
     {
@@ -50,8 +50,8 @@ export default function ProjectsSection() {
       period: "Mar. 2024 - Jun. 2024",
       description: "Using deep learning approach to answer GSAT questions with dataset from previous years. The model is trained to predict the correct answer with high accuracy.",
       slug: "gsat-test-taker",
-      slug_not_done: true,
-      github: "https://github.com/DebbieHuang35117/GSAT-test-taker.git",
+      //slug_not_done: true,
+      //github: "https://github.com/DebbieHuang35117/GSAT-test-taker.git",
       tags: ["Deep Learning", "NLP", "Python", "TensorFlow"]
     },
     {
@@ -59,8 +59,8 @@ export default function ProjectsSection() {
       period: "Sept. 2023 - Jan. 2024",
       description: "Developing a platform for students to acquaint with one another and forming study groups. Involving front-end and back-end techniques to construct a web system with over 10,000 students' data. ",
       slug: "study-connect",
-      slug_not_done: true,
-      github: "https://github.com/namwoam/study-connect",
+      //slug_not_done: true,
+      //github: "https://github.com/namwoam/study-connect",
       tags: ["Database", "MongoDB", "SQL", "Web Development", "React", "Node.js"]
     },
     {
@@ -68,8 +68,8 @@ export default function ProjectsSection() {
       period: "Sept. 2023 - Jan. 2024",
       description: "Predicting election results using GPT-3.5 for data labeling and analyzing public sentiment through social media.",
       slug: "election-prediction",
-      slug_not_done: true,
-      website: "https://www.dropbox.com/scl/fi/wgk1vkm7c2dnhyrju7rec/.pdf?rlkey=h4x0sl2n7uljtlqzll731acub&st=cg9emwrl&dl=0",
+      //slug_not_done: true,
+      //website: "https://www.dropbox.com/scl/fi/wgk1vkm7c2dnhyrju7rec/.pdf?rlkey=h4x0sl2n7uljtlqzll731acub&st=cg9emwrl&dl=0",
       tags: ["GPT-3.5", "Sentiment Analysis", "NLP"]
     }
   ];
@@ -85,17 +85,18 @@ export default function ProjectsSection() {
       <div className="h-px bg-gray-200 mb-6" />
       <div className="grid grid-cols-1 gap-6">
         {projects.map((project) => (
-          <Link 
-            href={project.slug_not_done && project.github ? (project.github) : (project.website ? project.website : `/projects/${project.slug}`)} 
-            key={project.slug}
-            className="block group"
-          >
-            <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-6 transition-all duration-300 hover:shadow-lg hover:border-gray-300 dark:hover:border-gray-600">
+          //<Link 
+          //  href={{/*project.slug_not_done && project.github ? (project.github) : (project.website ? project.website : `/projects/${project.slug}`)*/}} 
+          //  key={project.slug}
+          //  className="block group"
+          //>
+            <div key={project.slug} className="border border-gray-200 dark:border-gray-700 rounded-lg p-6 transition-all duration-300 hover:shadow-lg hover:border-gray-300 dark:hover:border-gray-600">
               <div className="flex justify-between items-start mb-2">
                 <h3 className="text-xl font-semibold group-hover:text-blue-600 dark:group-hover:text-blue-400">
                   {project.title}
                 </h3>
-                {project.github && (
+                {/*
+                </div>{project.github && (
                    <div 
                    onClick={(e) => handleGithubClick(e, project.github)}
                    className="relative z-10 bg-white p-2 rounded-md shadow-md -ml-1 cursor-pointer"
@@ -110,6 +111,7 @@ export default function ProjectsSection() {
                  </div>
                     )
                 }
+                */}
                 <span className="text-sm text-gray-500">{project.period}</span>
               </div>
               <p className="text-gray-600 dark:text-gray-300 mb-4">
@@ -126,7 +128,7 @@ export default function ProjectsSection() {
                 ))}
               </div>
             </div>
-          </Link>
+          //</Link>
         ))}
       </div>
     </div>
